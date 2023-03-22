0cf1ae28895bd40678bfa5328fcd54b1241285ca
Vitaly <vasilvvmain@gmail.com>
2023-03-22 23:19:40 +0100

    Rebuilt main menu page, added settings page.

---
+++ UnicornOneGame/Assets/Code/MonoBehaviours/MainMenuSceneLogic.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnicornOne.ScriptableObjects;
+using UnicornOne.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UnicornOne.MonoBehaviours
+{
+    public class MainMenuSceneLogic : MonoBehaviour
+    {
+        [SerializeField] private Canvas _backgroundCanvas;
+        [SerializeField] private Canvas _topCanvas;
+        [SerializeField] private Canvas _navbarCanvas;
+        [SerializeField] private Canvas _contentCanvas;
+        [SerializeField] private Canvas _pageCanvas;
+
+        [SerializeField] private MainMenuViewScript _mainMenuViewScript;
+        [SerializeField] private SettingsViewScript _settingsViewScript;
+
+        [SerializeField] private Level[] _availableLevels;
+
+        private AsyncOperation _gameSceneLoadingOperation = null;
+
+        private void Start()
+        {
+            _mainMenuViewScript.SetAvailableLevels(_availableLevels);
+            _mainMenuViewScript.PlayButtonClicked += OnPlayButtonClicked;
+
+            _settingsViewScript.BackButtonClicked += SettingsViewBackButtonClick;
+        }
+
+        private void OnDestroy()
+        {
+            _settingsViewScript.BackButtonClicked -= SettingsViewBackButtonClick;
+
+            _mainMenuViewScript.PlayButtonClicked -= OnPlayButtonClicked;
+        }
+
+        private void Update()
+        {
+        }
+
+        public void OnSettingButtonClick()
+        {
+            _topCanvas.gameObject.SetActive(false);
+            _navbarCanvas.gameObject.SetActive(false);
+            _contentCanvas.gameObject.SetActive(false);
+            _pageCanvas.gameObject.SetActive(true);
+
+            _settingsViewScript.gameObject.SetActive(true);
+        }
+
+        private void OnPlayButtonClicked()
+        {
+            if (_gameSceneLoadingOperation != null)
+            {
+                return;
+            }
+
+            // Clean previous value
+            SharedData.SelectedLevel = null;
+
+            var selectedOption = _mainMenuViewScript.SelectedLevelData;
+            SharedData.SelectedLevel = _availableLevels.FirstOrDefault(l => l.name == selectedOption.text);
+            if (SharedData.SelectedLevel == null)
+            {
+                SharedData.SelectedLevel = _availableLevels.First();
+            }
+
+            _gameSceneLoadingOperation = SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Single);
+        }
+
+        private void SettingsViewBackButtonClick()
+        {
+            _topCanvas.gameObject.SetActive(true);
+            _navbarCanvas.gameObject.SetActive(true);
+            _contentCanvas.gameObject.SetActive(true);
+            _pageCanvas.gameObject.SetActive(false);
+
+            _settingsViewScript.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ UnicornOneGame/Assets/Code/MonoBehaviours/SettingsViewScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace UnicornOne.MonoBehaviours
+{
+	public class SettingsViewScript : MonoBehaviour
+	{
+		public event Action BackButtonClicked; 
+
+		public void OnBackButtonClick()
+		{
+			BackButtonClicked?.Invoke();
+        }
+	}
+}

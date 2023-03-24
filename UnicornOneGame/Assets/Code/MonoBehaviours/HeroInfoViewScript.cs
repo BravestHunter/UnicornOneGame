863442e6715cc424298e0f0bcf3d7145dc1d9f45
Vitaly <vasilvvmain@gmail.com>
2023-03-24 23:39:06 +0100

    Added hero 3D model preview at hero info page.

--- UnicornOneGame/Assets/Code/MonoBehaviours/HeroInfoViewScript.cs
+++ UnicornOneGame/Assets/Code/MonoBehaviours/HeroInfoViewScript.cs
@@ -10,12 +10,15 @@ namespace UnicornOne.MonoBehaviours
 	public class HeroInfoViewScript : MonoBehaviour
 	{
         [SerializeField] private TMP_Text _title;
+        [SerializeField] private HeroPreviewScript _previewScript;
 
         public event Action BackButtonClicked;
 
         public void SetHero(Hero hero)
         {
             _title.text = hero.name;
+
+            _previewScript.SetHero(hero);
         }
 
         public void OnBackButtonClick()
---
+++ UnicornOneGame/Assets/Code/MonoBehaviours/HeroPreviewScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnicornOne.ScriptableObjects;
+
+namespace UnicornOne.MonoBehaviours
+{
+	public class HeroPreviewScript : MonoBehaviour
+	{
+		[SerializeField] private GameObject _container;
+
+		public void SetHero(Hero hero)
+		{
+			foreach (Transform child in _container.transform)
+			{
+				Destroy(child.gameObject);
+			}
+
+			var heroGameObject = Instantiate(hero.PrefabInfo.Prefab);
+			heroGameObject.transform.SetParent(_container.transform, false);
+        }
+	}
+}

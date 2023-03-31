2b9001c2ad352816038085ce1f5449905c1efebb
Vitaly <vasilvvmain@gmail.com>
2023-03-31 16:43:47 +0200

    Added tile and tilePath models.

---
+++ UnicornOneGame/Assets/Code/Board/Tile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnicornOne.Board
+{
+    [CreateAssetMenu(fileName = "Tile", menuName = "Custom/Board/Tile")]
+    public class Tile : ScriptableObject
+    {
+        public Color Color;
+
+        // All tile tada including tile script
+    }
+}
---
+++ UnicornOneGame/Assets/Code/Board/TilePath.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnicornOne.Board
+{
+    [CreateAssetMenu(fileName = "TilePath", menuName = "Custom/Board/TilePath")]
+    public class TilePath : ScriptableObject
+    {
+        [Serializable]
+        public struct TileEntry
+        {
+            public Vector3Int _position;
+            public Tile _tile;
+        }
+
+        public TileEntry[] Path;
+    }
+}
